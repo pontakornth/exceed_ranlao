cf8ef930517f82d500838be31853336b776f0b95
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-02-17 15:35:55 +0700

    test: Add test for demonstration

--- ranlao/tests.py
+++ ranlao/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+from ranlao.models import Table
+
+
+class TableViewTest(APITestCase):
+    def setUp(self) -> None:
+        self.table = Table.objects.create(table_number=1)
+
+    def test_call_staff(self):
+        url = reverse('call_staff', args=[1])
+        self.client.post(url)
+        # Refresh from DB is required as we mutated it.
+        self.table.refresh_from_db()
+        self.assertTrue(self.table.is_calling)
+
+    def test_complete_order(self):
+        url = reverse('complete_order', args=[1])
+        self.client.post(url)
+        self.table.refresh_from_db()
+        self.assertFalse(self.table.is_calling)
