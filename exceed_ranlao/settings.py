f973c7656102b1d90847d10ca6a6ce1f955374e6
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-02-17 16:23:34 +0700

    feat: Add token authentication

--- exceed_ranlao/settings.py
+++ exceed_ranlao/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'ranlao'
+    'rest_framework.authtoken',
+    'ranlao',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest framework setting.
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
