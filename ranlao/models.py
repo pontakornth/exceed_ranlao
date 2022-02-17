ad11a3f047f2dba8e4504b73e49d0a13327f95c5
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-02-17 17:32:03 +0700

    feat: Add model for recording logs

--- ranlao/models.py
+++ ranlao/models.py
@@ -4,6 +4,13 @@ from django.db import models
 
 # Create your models here.
 class Table(models.Model):
+    """Table at the pub and its status."""
     table_number = models.IntegerField(validators=[MinValueValidator(1)], null=False, unique=True)
     # is_calling: Call the staff.
     is_calling = models.BooleanField(default=False, null=False)
+
+
+class VisitorLog(models.Model):
+    """Log of visitor by hour."""
+    log_time = models.DateTimeField(null=False)
+    amount = models.IntegerField(validators=[MinValueValidator(0)], null=False)
