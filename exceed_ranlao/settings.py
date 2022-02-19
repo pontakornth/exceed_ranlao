5e5aea1e16560e8adbbf727b68bef4381666d6c0
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-02-19 17:30:23 +0700

    chore: Add allow host for testserver

--- exceed_ranlao/settings.py
+++ exceed_ranlao/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY', default='PLEASE_SETUP_SECRET_KEY_IT_IS_EASY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,testserver', cast=Csv())
 
 # Application definition
 
