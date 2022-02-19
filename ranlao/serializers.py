362ccbb7ed7cd9d0bf03b190d5256a3be284e3d4
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-02-19 14:18:12 +0700

    dev: Add log serializer for easy viewing

--- ranlao/serializers.py
+++ ranlao/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Table
+from .models import Table, VisitorLog
 
 
 class TableSerializer(ModelSerializer):
     class Meta:
         model = Table
         fields = ('table_number', 'is_calling')
+
+
+class LogSerializer(ModelSerializer):
+    class Meta:
+        model = VisitorLog
+        fields = ('log_time', 'amount')
